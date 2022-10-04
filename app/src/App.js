6be43dba2764f494e504f7e18ef2cddda1dd7ff6
= <=>
2022-10-04 15:17:55 +0300

    :wrench: Handling NoMatch Routes

--- app/src/App.js
+++ app/src/App.js
@@ -4,15 +4,16 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import OrderSummary from './components/OrderSummary';
-
+import NoMatch from './components/NoMatch';
 function App() {
   return (
     <div className="App">
      <Navbar />
      <Routes>
-      <Route path='/' element={<Home />}>Home</Route>
-      <Route path='about' element={<About />}>About</Route>
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<About />} />
       <Route path='order-summary' element={<OrderSummary />}/>
+      <Route path='*' element={<NoMatch />}/>
      </Routes>
     </div>
   );
