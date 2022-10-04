837a59be28386a53a6d678b83623f976d82a2500
= <=>
2022-10-04 10:51:00 +0300

    :bookmark: using Link component for client side navigation

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+
 function App() {
   return (
     <div className="App">
+     <Navbar />
      <Routes>
       <Route path='/' element={<Home />}>Home</Route>
-      <Route path='/about' element={<About />}>About</Route>
+      <Route path='about' element={<About />}>About</Route>
      </Routes>
     </div>
   );
