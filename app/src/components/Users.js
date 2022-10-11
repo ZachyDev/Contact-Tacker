65c2805ccf320884c48b3f279c9c5df6fb090db5
= <=>
2022-10-11 10:36:06 +0300

    :zap: added a nested route with dynamic route params

--- app/src/components/Users.js
+++ app/src/components/Users.js
@@ -2,22 +2,24 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import { useSearchParams } from 'react-router-dom';
 
-function Users() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const showActiveUsers = searchParams.get('filter') === 'active';
 
+function Users() {
+
+const [ searchParams, setSearchParams ] = useSearchParams();
+const setActiveUser = searchParams.get('filter') === 'active';
   return (
     <div>
         <h2>User1</h2>
         <h2>User2</h2>
         <h2>User3</h2>
         <div>
-          <Button variant='success' onClick={() => setSearchParams({ filter: 'active' })}>Active Users</Button>
+          <Button variant='success' onClick={() => setSearchParams({ filter: 'active' })}>Active users</Button>
           <Button variant='danger' onClick={() => setSearchParams({})}>Reset Filter</Button>
-        </div>
+        </div>   
         {
-          showActiveUsers ? (<h5>Showing active users</h5>) : (
-            <h5>Showing all users</h5>
+          setActiveUser ? (<h4>Showing active users</h4>) :
+          (
+            <h4>Showing all users</h4>
           )
         }
     </div>
