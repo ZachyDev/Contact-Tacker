9407c9edfe8f6b9765b657f2e6e8a39e45380238
= <=>
2022-10-11 10:58:55 +0300

    :pushpin: React lazy loading...

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import Card from 'react-bootstrap/Card';
 function About() {
   return (
     <div>
-        <h1>About</h1>
+       <Card style={{
+        'width':'50%',
+        'margin': '20px auto',
+        'padding': '20px'
+       }}>
+        <h2>About</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque repellendus in commodi? Aperiam, ut reiciendis? Aliquid at, nihil voluptas accusamus quam ad inventore esse earum dolore excepturi. Tenetur, blanditiis necessitatibus.</p>
+       </Card>
     </div>
   )
 }
